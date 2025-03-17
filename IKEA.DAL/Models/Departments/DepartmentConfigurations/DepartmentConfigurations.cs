10b59dc5256bd3997be2bd61c0d0199f0f696501
othman <01004634364a@gmail.com>
2025-03-17 18:36:22 +0200

    02-Department model with Confgurations

---
+++ IKEA.DAL/Models/Departments/DepartmentConfigurations/DepartmentConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Models.Departments.DepartmentConfigurations
+{
+    class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+            builder.Property(D => D.Name).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(D => D.Code).HasColumnType("varchar(20)").IsRequired();
+            builder.Property(D => D.CreatedOn).HasDefaultValueSql("Getdate()");
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("Getdate()");
+
+
+        }
+    }
+}
---
+++ IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IKEA.DAL.Models.Dapartments;
+using IKEA.DAL.Models.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Persistance.Data
+{
+    class ApplicationDbContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
