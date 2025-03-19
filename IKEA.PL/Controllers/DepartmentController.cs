ae4b40cccf070a8d4be4bd0af3cabda3f27ee5b7
othman <01004634364a@gmail.com>
2025-03-19 15:13:39 +0200

    02-Start Department Controller

---
+++ IKEA.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,16 @@
+﻿using IKEA.BLL.Services.DepartmentServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKEA.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private IDepartmentServices departmentServices;
+        public DepartmentController(IDepartmentServices _departmentServices)
+        => departmentServices = _departmentServices;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
