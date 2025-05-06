07e072080ff1837aa2fd67ff6723ecbf7f5caa49
othman <01004634364a@gmail.com>
2025-05-06 20:13:40 +0300

    01-How To Deal With Attachments!

---
+++ IKEA.BLL/Common/Services/Attachments/AttachmentServices.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.BLL.Common.Services.Attachments
+{
+    public class AttachmentServices : IAttachmentServices
+    {
+        private readonly List<string> AllowedExtentions = new List<string>() { ".jpg", ".png", ".jpeg" };
+        private const int FileMaximumSiz = 2_097_152;
+        public string UplodImage(IFormFile File, string FolderName)
+        {
+            var fileExtention = Path.GetExtension(File.FileName);
+            if (!AllowedExtentions.Contains(fileExtention))
+                throw new Exception("Invalid File Extention");
+            if (File.Length > FileMaximumSiz)
+                throw new Exception("Invalid File Size, Over our Range!!");
+
+            var FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", FolderName);
+            if (!Directory.Exists(FolderPath))
+                Directory.CreateDirectory(FolderPath);
+
+            var FileName = $"{Guid.NewGuid()}_{File.FileName}";
+            var FilPath = Path.Combine(FolderPath, FileName);
+            using var fs = new FileStream(FilPath, FileMode.Create);
+            File.CopyTo(fs);
+            return FileName;
+
+        }
+        public bool DeleteImage(string FilePath)
+        {
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ IKEA.BLL/Common/Services/Attachments/IAttachmentServices.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.BLL.Common.Services.Attachments
+{
+    public interface IAttachmentServices
+    {
+        public string UplodImage(IFormFile File,string FolderName);
+        public bool DeleteImage(string FilePath);
+
+    }
+}
